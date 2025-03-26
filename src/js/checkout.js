28fec7784db5136c15a908df35bf08a51ce99b29
mwana20 <phillipmwanaweika@gmail.com>
2025-03-26 11:36:34 +0300

    Week4 -Phillip

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,5 +1,5 @@
 import { loadHeaderFooter } from "./utils.mjs";
-import { CheckoutProcess } from "./CheckoutProcess.mjs";
+import CheckoutProcess from "./CheckoutProcess.mjs";  // Default import for CheckoutProcess
 
 loadHeaderFooter();
 
--- src/js/product.js
+++ src/js/product.js
@@ -1,1 +1,11 @@
-Products mergeAlias
+import { getParam, loadHeaderFooter } from "./utils.mjs";
+import ExternalServices from "./ExternalServices.mjs";
+import ProductDetails from "./ProductDetails.mjs";
+
+loadHeaderFooter();
+
+const dataSource = new ExternalServices("tents");
+const productID = getParam("product");
+
+const product = new ProductDetails(productID, dataSource);
+product.init();
